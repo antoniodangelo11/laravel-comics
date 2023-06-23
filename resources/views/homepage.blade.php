c8a781243db2fc895e04b6347463f032958d6a72
antoninodangelo11 <reapercsp@gmail.com>
2023-06-23 15:49:00 +0200

    import array

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,11 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Laravel Comics</title>
         @vite('resources/js/app.js')
     </head>
     <body>
         <h1 class="text-center">Ciao da vite</h1>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
     </body>
 </html>
